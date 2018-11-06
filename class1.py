9ac61bf46d906d2900c907c8f1f6774ebec98697
RAINANITIN <44721158+RAINANITIN@users.noreply.github.com>
2018-11-06 18:08:06 +0530

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,14 @@
+for x in range(1):
+    for y in range(1,4):
+        print(y,end="")
+    print()
+
+    for y in range(4,7):
+        print(y,end="")
+    print()    
+
+    for y in range(7,10):
+        print(y,end="")
+    print()    
+
+
---
+++ class2.py
@@ -0,0 +1,5 @@
+name="sathya"
+for y in name:
+    for x in range(5):
+        print(y,end="")
+    print()
